dbf16769ba8b514a90a8d829feece21b2616061c
Clarence Ho <clarence@skywidesoft.com>
2017-07-01 16:51:15 +0800

    Minor updates

--- docker-contact-web/src/environments/environment.prod.ts
+++ docker-contact-web/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiEndpoint: 'http://pwd10-0-46-3-9001.host1.labs.play-with-docker.com'
+  apiEndpoint: 'http://pwd10-0-19-3-9001.host2.labs.play-with-docker.com'
 };
